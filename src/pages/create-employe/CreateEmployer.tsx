52c15725a337f6128275db9b329b312e98ac1951
DavyHaYehoudi <davyhayehoudi@gmail.com>
2024-10-17 17:08:03 +0200

    change input state in select input region

--- src/pages/create-employe/CreateEmployer.tsx
+++ src/pages/create-employe/CreateEmployer.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Modal from "@davy-dev/react-modal-plugin"; // Assurez-vous que le chemin est correct
+import Modal from "@davy-dev/react-modal-plugin";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { formSchema } from "../../schemas/formSchema";
@@ -12,17 +12,17 @@ import SelectField from "../../shared/inputs/SelectField";
 
 const CreateEmployer = () => {
   const [isOpen, setIsOpen] = useState(false);
-  
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       firstName: "",
       lastName: "",
-      dateOfBirth: undefined, 
+      dateOfBirth: undefined,
       startDate: undefined,
       street: "",
       city: "",
-      state: "",
+      region: "",
       zipCode: "",
       department: "",
     },
@@ -42,17 +42,65 @@ const CreateEmployer = () => {
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <h2 className="text-2xl font-bold">Créer un employé</h2>
 
-          <ClassicInputField name="firstName" label="Prénom" placeholder="John" />
+          <ClassicInputField
+            name="firstName"
+            label="Prénom"
+            placeholder="John"
+          />
           <ClassicInputField name="lastName" label="Nom" placeholder="Doe" />
           <DatePickerField name="dateOfBirth" label="Date de naissance" />
           <DatePickerField name="startDate" label="Date de début" />
 
           <fieldset className="space-y-4">
             <legend className="text-lg font-semibold">Adresse</legend>
-            <ClassicInputField name="street" label="Rue" placeholder="123 Main St" />
+            <ClassicInputField
+              name="street"
+              label="Rue"
+              placeholder="123 Main St"
+            />
             <ClassicInputField name="city" label="Ville" placeholder="Paris" />
-            <ClassicInputField name="state" label="État" placeholder="Île-de-France" />
-            <ClassicInputField name="zipCode" label="Code Postal" type="number" placeholder="75000" />
+            {/* <ClassicInputField name="state" label="État" placeholder="Île-de-France" /> */}
+            <SelectField
+              name="region"
+              label="Région"
+              options={[
+                {
+                  value: "Auvergne-Rhône-Alpes",
+                  label: "Auvergne-Rhône-Alpes",
+                },
+                {
+                  value: "Bourgogne-Franche-Comté",
+                  label: "Bourgogne-Franche-Comté",
+                },
+                { value: "Bretagne", label: "Bretagne" },
+                { value: "Centre-Val de Loire", label: "Centre-Val de Loire" },
+                { value: "Corse", label: "Corse" },
+                { value: "Grand Est", label: "Grand Est" },
+                { value: "Hauts-de-France", label: "Hauts-de-France" },
+                { value: "Île-de-France", label: "Île-de-France" },
+                { value: "Normandie", label: "Normandie" },
+                { value: "Nouvelle-Aquitaine", label: "Nouvelle-Aquitaine" },
+                { value: "Occitanie", label: "Occitanie" },
+                { value: "Pays de la Loire", label: "Pays de la Loire" },
+                {
+                  value: "Provence-Alpes-Côte d'Azur",
+                  label: "Provence-Alpes-Côte d'Azur",
+                },
+                { value: "Guadeloupe", label: "Guadeloupe" },
+                { value: "Martinique", label: "Martinique" },
+                { value: "Guyane", label: "Guyane" },
+                { value: "La Réunion", label: "La Réunion" },
+                { value: "Mayotte", label: "Mayotte" },
+              ]}
+              placeholder="Sélectionner une région"
+            />
+
+            <ClassicInputField
+              name="zipCode"
+              label="Code Postal"
+              type="number"
+              placeholder="75000"
+            />
           </fieldset>
 
           <SelectField
@@ -68,7 +116,10 @@ const CreateEmployer = () => {
             placeholder="Sélectionner"
           />
 
-          <Button type="submit" className="bg-blue-500 hover:bg-blue-600 rounded text-[whitesmoke]">
+          <Button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 rounded text-[whitesmoke]"
+          >
             Enregistrer
           </Button>
         </form>
--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -11,7 +11,7 @@ export const formSchema = z.object({
   }),
   street: z.string().nonempty({ message: "La rue est requise" }),
   city: z.string().nonempty({ message: "La ville est requise" }),
-  state: z.string().nonempty({ message: "L'état est requis" }),
+  region: z.string().nonempty({ message: "La région est requise" }),
   zipCode: z.string().nonempty({ message: "Le code postal est requis" }),
   department: z.string().nonempty({ message: "Le département est requis" }),
 });
